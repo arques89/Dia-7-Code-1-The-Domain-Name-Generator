257c875b65383bce93c1ae02690f0fc7e332016a
arques89 <arquesjavier@gmail.com>
2022-05-31 15:31:59 +0000

    ejercicio finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["the", "our"];
+  let adj = ["massive", "lil", "dead"];
+  let noun = ["nemesis", "hero"];
+  let domain = [".com", ".net"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainName.push(pronoun[i] + adj[j] + noun[k] + domain[l] + "<br/>");
+        }
+      }
+    }
+  }
+
+  document.getElementById("btn").innerHTML = domainName;
 };
